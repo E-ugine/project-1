874e3e91115c65dd9d6269add563f09cdc679e2d
EugineAgolla <agollaeugine@gmail.com>
2024-07-16 16:10:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const header = document.getElementsByClassName('header');
+const logo = document.getElementsByClassName('logo');
+const navLinks = document.getElementsByClassName('nav-links');
+const actions = document.getElementsByClassName('actions');
+const hero = document.getElementsByClassName('hero');
+const images = document.getElementsByClassName('images');
+const searchBar = document.getElementsByClassName('search-bar');
+const topDestinations = document.getElementsByClassName('top-destinations');
+const grid = document.getElementsByClassName('grid');
+const usefulInfo = document.getElementsByClassName('useful-info');
+
+});
